e2cec9d6e49cd1ab9611def29e9281cc073fa65c
imortalout <imortalout@gmail.com>
2020-04-25 00:52:39 -0300

    bug fixing

--- src/components/ProgressTracking/index.js
+++ src/components/ProgressTracking/index.js
@@ -4,18 +4,13 @@ import { Container, DotSelected, DotNo } from './styles'
 
 export default function ProgressTracking(props) {
 
-
 	function renderDots(){
 		let { amount, position } = props;
-
-		console.log(amount)
-		console.log(position)
 
 		var circles = []
 		for (let i = 1; i <= amount; i++){
 			if(i === position){
 				circles.push(<DotSelected/>)
-				console.log('Chamo aqui pow')
 			} else{
 				circles.push(<DotNo/>)
             }
